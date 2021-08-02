b1c1951b91b2302357d252051a8530a04fa820b4
vkaltae9 <vikram.kalta@e9.co.uk>
2021-08-03 00:15:43 +0530

    fix: test

--- verifyCommit.js
+++ verifyCommit.js
@@ -18,9 +18,7 @@ if (!commitRE.test(msg)) {
 // const msg = require('fs')
 //     .readFileSync(msgPath, 'utf-8')
 //     .trim();
-
 // const commitRE = /^(revert: |Merge.+)|(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|chore|types|wip|merge)(\(.+\))?: .+/;
-
 // if (!commitRE.test(msg)) {
 //     console.error(`  ${chalk.bgRed.white(' ERROR ')} ${chalk.red('invalid commit message format.')}\n\n`);
 //     process.exit(1);
