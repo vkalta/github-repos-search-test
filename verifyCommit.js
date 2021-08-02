f4f58ed24f6bf4f8dc59a57bbacb7a9c2902c7af
vkaltae9 <vikram.kalta@e9.co.uk>
2021-08-03 00:11:43 +0530

    fix: test

--- verifyCommit.js
+++ verifyCommit.js
@@ -11,3 +11,17 @@ if (!commitRE.test(msg)) {
     console.error(`ERROR: invalid commit message format`);
     process.exit(1);
 }
+
+/* eslint no-console: 0 */
+// const chalk = require('chalk');
+// const msgPath = process.env.HUSKY_GIT_PARAMS;
+// const msg = require('fs')
+//     .readFileSync(msgPath, 'utf-8')
+//     .trim();
+
+// const commitRE = /^(revert: |Merge.+)|(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|chore|types|wip|merge)(\(.+\))?: .+/;
+
+// if (!commitRE.test(msg)) {
+//     console.error(`  ${chalk.bgRed.white(' ERROR ')} ${chalk.red('invalid commit message format.')}\n\n`);
+//     process.exit(1);
+// }
